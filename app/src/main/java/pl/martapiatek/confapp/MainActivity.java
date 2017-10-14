1d616bc187504d6b05f7fd375fab4b5479125198
MartaPiatek <martusia.piatek@gmail.com>
2017-10-14 19:03:48 +0200

    Wyświetlanie ekranu powitalnego, closes #2

--- app/src/main/java/pl/martapiatek/confapp/MainActivity.java
+++ app/src/main/java/pl/martapiatek/confapp/MainActivity.java
@@ -1,14 +1,20 @@
 package pl.martapiatek.confapp;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable{
 
     private Button btnAgenda, btnMap, btnFacebook;
+    private Dialog splashDialog;
+    private Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        showSplashScreen();
+        handler = new Handler();
+        AsyncTask.execute(this);
 
         btnAgenda = (Button) findViewById(R.id.btnAgenda);
 
@@ -35,5 +44,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        dismissSplashScreen();
+    }
+
+    private void showSplashScreen() {
+        splashDialog = new Dialog(this, R.style.splash_screen);
+        splashDialog.setContentView(R.layout.activity_splash);
+        splashDialog.setCancelable(false);
+        splashDialog.show();
+    }
+
+    private void dismissSplashScreen() {
+        if (splashDialog != null) {
+            splashDialog.dismiss();
+            splashDialog = null;
+        }
+    }
+    @Override
+    public void run() {
+        handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    dismissSplashScreen();
+                                }
+                            }, 5000
+        );
     }
 }
