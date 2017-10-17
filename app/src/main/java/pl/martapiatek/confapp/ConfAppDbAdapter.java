5981fd4ca3f4c68a556432f7c728ba902f869aa5
MartaPiatek <martusia.piatek@gmail.com>
2017-10-17 13:26:51 +0200

    Zapis w bazie danych informacji o wydarzeniach, closes #10

--- app/src/main/java/pl/martapiatek/confapp/ConfAppDbAdapter.java
+++ app/src/main/java/pl/martapiatek/confapp/ConfAppDbAdapter.java
@@ -18,6 +18,13 @@ public class ConfAppDbAdapter {
     public static final String COL_SPEAKER_TITLE = "speakerTitle";
     public static final String COL_SPEAKER_DESCRIPTION = "speakerDescription";
 
+    public static final String COL_EVENT_ID = "_id";
+    public static final String COL_EVENT_DATE = "date";
+    public static final String COL_EVENT_LOCATION = "location";
+    public static final String COL_EVENT_TITLE = "eventTitle";
+    public static final String COL_EVENT_DESCRIPTION = "eventDescription";
+    public static final String COL_EVENT_SPEAKER = "eventSpeaker";
+
     //indeksy
 
     public static final int INDEX_SPEAKER_ID = 0;
@@ -25,6 +32,13 @@ public class ConfAppDbAdapter {
     public static final int INDEX_SPEAKER_LAST_NAME = INDEX_SPEAKER_ID + 2;
     public static final int INDEX_SPEAKER_TITLE = INDEX_SPEAKER_ID + 3;
     public static final int INDEX_SPEAKER_DESCRIPTION = INDEX_SPEAKER_ID + 4;
+
+    public static final int INDEX_EVENT_ID = 0;
+    public static final int INDEX_EVENT_DATE = INDEX_EVENT_ID + 1;
+    public static final int INDEX_EVENT_LOCATION = INDEX_EVENT_ID + 2;
+    public static final int INDEX_EVENT_TITLE = INDEX_EVENT_ID + 3;
+    public static final int INDEX_EVENT_DESCRIPTION = INDEX_EVENT_ID + 4;
+    public static final int INDEX_EVENT_SPEAKER = INDEX_EVENT_ID + 5;
 
     //dziennik zdarzeń
     public static final String TAG = "ConfAppDbAdapter";
@@ -35,6 +49,7 @@ public class ConfAppDbAdapter {
     private static final String DATABASE_NAME = "dba_confApp";
 
     private static final String TABLE_SPEAKER_NAME = "tbl_speaker";
+    private static final String TABLE_EVENT_NAME = "tbl_event";
 
     private static final int DATABASE_VERSION = 1;
 
@@ -48,6 +63,16 @@ public class ConfAppDbAdapter {
                      COL_SPEAKER_LAST_NAME + " TEXT, " +
                      COL_SPEAKER_TITLE + " TEXT, " +
                      COL_SPEAKER_DESCRIPTION + " TEXT ); "
+            ;
+
+    private static final String CREATE_TABLE_EVENT =
+            "CREATE TABLE if not exists " + TABLE_EVENT_NAME + " ( " +
+                    COL_EVENT_ID + " INTEGER PRIMARY KEY autoincrement, " +
+                    COL_EVENT_DATE + " TEXT, " +
+                    COL_EVENT_LOCATION + " TEXT, " +
+                    COL_EVENT_TITLE + " TEXT, " +
+                    COL_EVENT_DESCRIPTION + " TEXT, " +
+                    COL_EVENT_SPEAKER + " TEXT ); "
             ;
 
 
@@ -79,6 +104,9 @@ public class ConfAppDbAdapter {
 
             Log.w(TAG, CREATE_TABLE_SPEAKER);
             db.execSQL(CREATE_TABLE_SPEAKER);
+
+            Log.w(TAG, CREATE_TABLE_EVENT);
+            db.execSQL(CREATE_TABLE_EVENT);
         }
 
         @Override
@@ -87,6 +115,7 @@ public class ConfAppDbAdapter {
             Log.w(TAG, "Aktualizacja bazy danych z wersji "+ oldVersion + " do wersji " +
                     newVersion + " , co powoduje wyczyszczenie zawartości bazy danych.");
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_SPEAKER_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENT_NAME);
             onCreate(db);
         }
     }
@@ -111,6 +140,16 @@ public class ConfAppDbAdapter {
         return mDb.insert(TABLE_SPEAKER_NAME, null, values);
     }
 
+    public void createEvent(String date, String location, String title, String description, String speaker){
+        ContentValues values = new ContentValues();
+        values.put(COL_EVENT_DATE, date);
+        values.put(COL_EVENT_LOCATION, location);
+        values.put(COL_EVENT_TITLE, title);
+        values.put(COL_EVENT_DESCRIPTION, description);
+        values.put(COL_EVENT_SPEAKER, speaker);
+        mDb.insert(TABLE_EVENT_NAME, null, values);
+    }
+
     // ODCZYT
     public Speaker fetchSpeakerById(int id) {
 
@@ -129,11 +168,39 @@ public class ConfAppDbAdapter {
                 cursor.getString(INDEX_SPEAKER_DESCRIPTION) );
     }
 
+    public Event fetchEventById(int id) {
+
+        Cursor cursor = mDb.query(TABLE_EVENT_NAME, new String[]{COL_EVENT_ID,
+                        COL_EVENT_DATE, COL_EVENT_LOCATION, COL_EVENT_TITLE,
+                        COL_EVENT_DESCRIPTION, COL_EVENT_SPEAKER}, COL_EVENT_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        return new Event(
+                cursor.getInt(INDEX_EVENT_ID),
+                cursor.getString(INDEX_EVENT_DATE),
+                cursor.getString(INDEX_EVENT_LOCATION),
+                cursor.getString(INDEX_EVENT_TITLE),
+                cursor.getString(INDEX_EVENT_DESCRIPTION),
+                cursor.getString(INDEX_EVENT_SPEAKER)
+        );
+    }
 
     public Cursor fetchAllSpeakers(){
         Cursor mCursor = mDb.query(TABLE_SPEAKER_NAME, new String[]{COL_SPEAKER_ID,
                         COL_SPEAKER_FIRST_NAME, COL_SPEAKER_LAST_NAME, COL_SPEAKER_TITLE, COL_SPEAKER_DESCRIPTION},
                 null, null, null, null, COL_SPEAKER_FIRST_NAME);
+        if(mCursor != null)
+            mCursor.moveToFirst();
+
+        return mCursor;
+    }
+
+    public Cursor fetchAllEvents(){
+        Cursor mCursor = mDb.query(TABLE_EVENT_NAME, new String[]{COL_EVENT_ID,
+                        COL_EVENT_DATE, COL_EVENT_LOCATION, COL_EVENT_TITLE, COL_EVENT_DESCRIPTION, COL_EVENT_SPEAKER},
+                null, null, null, null, COL_EVENT_ID);
         if(mCursor != null)
             mCursor.moveToFirst();
 
@@ -147,6 +214,14 @@ public class ConfAppDbAdapter {
 
     public void deleteAllSpeakers(){
         mDb.delete(TABLE_SPEAKER_NAME, null, null);
+    }
+
+    public void deleteEventById(int nId){
+        mDb.delete(TABLE_EVENT_NAME, COL_EVENT_ID + "=?", new String[]{String.valueOf(nId)});
+    }
+
+    public void deleteAllEvents(){
+        mDb.delete(TABLE_EVENT_NAME, null, null);
     }
 
 }
---
+++ app/src/main/java/pl/martapiatek/confapp/Event.java
@@ -0,0 +1,88 @@
+package pl.martapiatek.confapp;
+
+
+import java.util.Date;
+
+public class Event {
+
+
+    private int id;
+    private String date;
+    private String location;
+    private String title;
+    private String description;
+    //private Speaker speaker;
+    private String speakerName;
+
+    public Event(int id, String date, String location, String title, String description, String speakerName) {
+        this.id = id;
+        this.date = date;
+        this.location = location;
+        this.title = title;
+        this.description = description;
+        this.speakerName = speakerName;
+    }
+    public Event( String date, String location, String title, String description, String speakerName) {
+
+        this.date = date;
+        this.location = location;
+        this.title = title;
+        this.description = description;
+        this.speakerName = speakerName;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSpeakerName() {
+        return speakerName;
+    }
+
+    public void setSpeakerName(String speakerName) {
+        this.speakerName = speakerName;
+    }
+
+    // public Speaker getSpeaker() {
+   //     return speaker;
+   // }
+
+   // public void setSpeaker(Speaker speaker) {
+    //    this.speaker = speaker;
+   // }
+}
