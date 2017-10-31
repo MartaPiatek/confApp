35a2f2de58ebf08e80388bc76d441e8376b098a7
MartaPiatek <martusia.piatek@gmail.com>
2017-10-31 19:16:00 +0100

    Implementacja zakładki Kontakt, closes #35

---
+++ app/src/main/java/pl/martapiatek/confapp/ContactActivity.java
@@ -0,0 +1,29 @@
+package pl.martapiatek.confapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+
+        Button btnMessage = (Button) findViewById(R.id.btnMessage);
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent startIntent = new Intent(getApplicationContext(),MailContactActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+    }
+}
