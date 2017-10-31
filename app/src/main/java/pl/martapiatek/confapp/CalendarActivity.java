b9e2c2a7ff221b2055eaee3df11cb8b23d37e783
MartaPiatek <martusia.piatek@gmail.com>
2017-10-31 19:04:17 +0100

    Dodawanie wydarzenia do terminarza, closes #18

---
+++ app/src/main/java/pl/martapiatek/confapp/CalendarActivity.java
@@ -0,0 +1,69 @@
+package pl.martapiatek.confapp;
+
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class CalendarActivity extends AppCompatActivity {
+
+        private ListView mListView;
+        private ConfAppDbAdapter mDbAdapter;
+        private ConfAppSimpleCursorAdapter mCursorAdapter;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_calendar);
+
+            mListView = (ListView) findViewById(R.id.events_list_view);
+            mListView.setDivider(null);
+            mDbAdapter = new ConfAppDbAdapter(this);
+            mDbAdapter.open();
+
+            //dodaj przykładowe dane
+            //  insertSomeEvents();
+
+            Cursor cursor = mDbAdapter.fetchAllCalendarEvents();
+
+            // z kolumn zdefiniowanych w bazie danych
+            String[] from = new String[]{
+                    ConfAppDbAdapter.COL_EVENT_DATE, ConfAppDbAdapter.COL_EVENT_LOCATION,
+                    ConfAppDbAdapter.COL_EVENT_TITLE
+
+            };
+
+            // do identyfikatorów widoków w układzie graficznym
+            int[] to = new int[]{
+                    R.id.row_date,
+                    R.id.row_location,
+                    R.id.row_title
+
+            };
+
+            mCursorAdapter = new ConfAppSimpleCursorAdapter(
+                    // kontekst
+                    CalendarActivity.this,
+                    // układ graficzny wiersza
+                    R.layout.events_rows,
+                    // kursor
+                    cursor,
+                    // z kolumn zdefiniowanych w bazie danych
+                    from,
+                    // do identyfikatorów widoków w układzie graficznym
+                    to,
+                    // znacznik - nieużywany
+                    0);
+
+            mListView.setAdapter(mCursorAdapter);
+
+
+
+        } // onCreate
+
+
+
+
+}
---
+++ app/src/main/java/pl/martapiatek/confapp/CalendarEvent.java
@@ -0,0 +1,60 @@
+package pl.martapiatek.confapp;
+
+
+public class CalendarEvent {
+
+
+    private int id;
+    private String date;
+    private String location;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public CalendarEvent(int id, String date, String location, String title) {
+        this.id = id;
+        this.date = date;
+        this.location = location;
+        this.title = title;
+    }
+
+    public CalendarEvent(String date, String location, String title) {
+        this.date = date;
+        this.location = location;
+        this.title = title;
+    }
+}
+
+
+
